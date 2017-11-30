fbbedb13374b4416962e36f542cb1454fc35c7a7
rdxi <rdxi@yandex.ru>
2017-11-30 21:53:39 +0700

    add slider js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-// show/hide sticky header
 $(document).ready(function() {
   stickyHeaderHandler();
   modalHandler();
-  sliderHandler();
+  sliderHandler.init(1);
 });
 
 var stickyHeaderHandler = function() {
   var header = $('.header');
+  var isWindowScrolledOnLoad = $(window).scrollTop() > 0;
 
-  if ($(window).scrollTop() > 0)  {
+  if (isWindowScrolledOnLoad)  {
     header.addClass('is-scrolling');
   }
 
-  $(window).scroll(function() {
-    if( $(window).scrollTop() > 0 ) {
+  $(window).on('scroll', function() {
+    var isWindowScrolled = $(window).scrollTop() > 0;
+
+    if (isWindowScrolled) {
       header.addClass('is-scrolling');
     } else {
       header.removeClass('is-scrolling');
@@ -25,30 +27,101 @@ var modalHandler = function() {
   var modalOverlay = $('.modal-overlay');
 
   $('.js-modal-show').on('click', function() {
-    modalOverlay.show();
+    modalOverlay.fadeIn(200);
   });
 
   $('.js-modal-hide').on('click', function(event) {
-    var target = $(event.target);
+    var targetHasClass = $(event.target).hasClass('js-modal-hide');
 
-    if (target.hasClass('js-modal-hide')) {
-      modalOverlay.hide();
+    if (targetHasClass) {
+      modalOverlay.fadeOut(200);
     }
   });
 };
 
+var sliderHandler = {
+  init: function(index) {
+    this.currentIndex = index || 0;
 
-var sliderHandler = function() {
-  // var currIndex = 1;
+    this.photo = $('.slider-photo');
+    this.currentPhotoClassName = 'slider-photo--current';
 
-  // on arrow clicks increase and decrease currIndex;
+    this.textElContainer = $('.slider-text-container-inner');
+    this.textEl = $('.slider-text');
+    this.textElWidth = this.textEl.width();
 
+    this.photoListeners();
+    this.arrowListeners();
 
-  // we got index of slide
-  // send index to function
-  // function adds class to photo with index, removes class from other photos
-  // function scrolls text to element with index
+    this.changePhoto();
+    this.changeText();
 
+    // this.autoScroll(5000);
+  },
 
+  photoListeners: function() {
+    var self = this;
 
+    self.photo.on('click', function() {
+      self.currentIndex = $(this).index();
+
+      self.changePhoto();
+      self.changeText();
+    });
+  },
+
+  arrowListeners: function() {
+    var self = this;
+
+    $('.slider-arrow').on('click', function() {
+      var isArrowRight = $(this).hasClass('slider-arrow--right');
+
+      if (isArrowRight) {
+        self.increaseCurrentIndex();
+      } else {
+        self.decreaseCurrentIndex();
+      }
+
+      self.changePhoto();
+      self.changeText();
+    });
+
+  },
+
+  changePhoto: function() {
+    var self = this;
+
+    self.photo.eq(self.currentIndex)
+         .addClass(this.currentPhotoClassName)
+         .siblings()
+         .removeClass(this.currentPhotoClassName);
+  },
+
+  changeText: function() {
+    var self = this;
+    var textElPosition = self.textElWidth * self.currentIndex;
+
+    self.textElContainer.css('transform', 'translateX('+ -textElPosition +'px)');
+  },
+
+  increaseCurrentIndex: function() {
+    var self = this;
+    self.currentIndex = (self.currentIndex < self.photo.length - 1) ? self.currentIndex + 1 : 0;
+  },
+
+  decreaseCurrentIndex: function() {
+    var self = this;
+    self.currentIndex = (self.currentIndex > 0) ? self.currentIndex - 1 : self.photo.length - 1;
+  },
+
+  autoScroll: function(interval) {
+    var self = this;
+    var scrollInterval = interval || 2000;
+
+    setInterval(function() {
+      self.increaseCurrentIndex();
+      self.changePhoto();
+      self.changeText();
+    }, scrollInterval);
+  }
 };
